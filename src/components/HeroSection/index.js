28cd238e6839e0f68e4ec1883f792b59e28585e1
LMaicelo <leonardomaicelo@gmail.com>
2020-12-30 22:59:28 -0500

    First Commit

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import Video from '../../videos/video.mp4';
+import {Button} from '../ButtonElements';
+import {
+    HeroContainer,
+    HeroBg, 
+    VideoBg,
+    HeroContent,
+    HeroH1,
+    HeroP,
+    HeroBtnWrapper,
+    ArrowForward,
+    ArrowRight
+} from './HeroElements';
+
+const HeroSection = () => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
+    return (
+        <HeroContainer>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4' /> 
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>Getting You World-Wide Leads</HeroH1>
+                <HeroP>
+                    Sign up today!
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to="sign-up" onMouseEnter={onHover}
+                    primary="true" dark="true">
+                        Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default HeroSection
---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,51 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Delegate Your Digital Marketing',
+    headline: 'Scale up your lead generation with us!',
+    description: 'Get access to our exclusive subcription list for the latest FinTech news.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Image',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Delegate Your Digital Marketing',
+    headline: 'Scale up your lead generation with us!',
+    description: 'Get access to our exclusive subcription list for the latest FinTech news.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-2.svg').default,
+    alt: 'Image',
+    dark: false,
+    primary: false,
+    darkText: true
+};
+
+export const homeObjThree = {
+    id: 'sign-up',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Delegate Your Digital Marketing',
+    headline: 'Scale up your lead generation with us!',
+    description: 'Get access to our exclusive subcription list for the latest FinTech news.',
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: require('../../images/svg-3.svg').default,
+    alt: 'Image',
+    dark: false,
+    primary: false,
+    darkText: true
+};
+
